573c471d210bc735dc1f836789bf4d42b837c3f2
MohamedAsik2288 <128294190+MohamedAsik2288@users.noreply.github.com>
2023-07-19 22:00:30 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./App.css";
+import { useState } from 'react';
+import JSONDATA from './MOCK_DATA.json';
+const App = () => {
+
+const [searchTerm,setSearchTerm] =useState('');
+
+  return (
+    <div className='App'>
+
+    <input type='text' placeholder='Search'
+    onChange={event=>{setSearchTerm(event.target.value)}}/>
+    
+    {JSONDATA.filter((val)=>  {
+      if(searchTerm == ""){
+        return  val
+      }else if (val.first_name.toLocaleLowerCase().includes(searchTerm.toLowerCase())) {
+        return val;
+      }
+
+    }).map((val,key) => {
+      return <div className='user' key={key}>{val.first_name}</div>
+
+    })}
+    
+    </div>
+  )
+}
+
+export default App
